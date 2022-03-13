8fd240f02b784db4859d933d9b9e96849099d207
guilhermeapgap <guilhermeapgap@hotmail.com>
2022-03-12 21:04:28 -0300

    atualizado

---
+++ MySQL/storage1.sql
@@ -0,0 +1,19 @@
+-- COMENTARIOS SÃO FEITOS DESSA FORMA
+/*COMENTARIO DE VARIAS LINHAS*/
+-- REMOVENDO DATA BASE COM O COMANDO: DROP DATABASE NOME;
+/*
+USE storage1
+CREATE DATABASE storage1;
+CREATE TABLE table1
+(
+	email VARCHAR(30),
+	senha VARCHAR(10)
+);
+DESCRIBE table1;
+DROP TABLE table1;
+INSERT INTO table1 ( email, senha )
+			VALUES ( 'guilherme@hotmail.com', '123456')
+
+TRUNCATE TABLE table1;(serve para limpar os dados da tabela)
+*/
+
