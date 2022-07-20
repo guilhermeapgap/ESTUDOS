89f527615935d39df8ec1200b15565b947d732b0
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2022-07-20 15:45:55 -0300

    estudos em node antes de sair

---
+++ node.js/9-sequelize/4-lendo-dados/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended:true,
+}),
+)
+
+app.use(express.json())//pegando o body em json
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')//template engine
+
+app.use(express.static('public'))//ponte para os arquivos estaticos
+
+app.get('/users/create', (req,res)=>{
+    res.render('adduser')
+})
+
+
+app.post('/users/create', function (req, res) {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+  
+    if (newsletter === 'on') {
+      newsletter = true
+    }else {
+        newsletter = false
+    }
+
+    User.create({ name, occupation, newsletter })
+  
+    res.redirect('/')
+  })
+  
+//home
+app.get('/', async (req,res)=>{
+
+    const users = await User.findAll({raw: true})
+
+    console.log(users)
+    res.render('home', {users: users})
+})
+
+
+conn.sync().then(()=>{
+   app.listen(3000) 
+}).catch((err)=> console.log(err))
+
---
+++ node.js/9-sequelize/5-resgatando-dados/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended:true,
+}),
+)
+
+app.use(express.json())//pegando o body em json
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')//template engine
+
+app.use(express.static('public'))//ponte para os arquivos estaticos
+
+app.get('/users/create', (req,res)=>{
+    res.render('adduser')
+})
+
+//lendo os dados do banco 
+app.post('/users/create', function (req, res) {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+  
+    if (newsletter === 'on') {
+      newsletter = true
+    }else {
+        newsletter = false
+    }
+
+    User.create({ name, occupation, newsletter })
+  
+    res.redirect('/')
+  })
+  
+//resgatando os dados do banco 
+app.get('/user/:id', async (req,res)=>{
+
+    const id = req.params.id
+    const user = await User.findOne({raw: true, where: {id: id}})
+
+    res.render('userview', {user})
+})
+
+//home
+app.get('/', async (req,res)=>{
+
+    const users = await User.findAll({raw: true})
+
+    console.log(users)
+    res.render('home', {users: users})
+})
+
+
+conn.sync().then(()=>{
+   app.listen(3000) 
+}).catch((err)=> console.log(err))
+
