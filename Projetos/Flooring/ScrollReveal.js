54d45097d83d7795e4bcdd6173d8d77abeba0f53
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-01-24 16:47:09 -0300

    website flooring

---
+++ Projetos/Flooring/ScrollReveal.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.container-services-1',{duration: 1000})
+sr.reveal('.animated-hr',{duration: 1000})
+sr.reveal('.container-section-2')
+sr.reveal('.container-section-3')
+sr.reveal('.container-section-4', {
+    rotate:{x:80, y:0, z:0},
+    duration: 1000,
+})
+sr.reveal('.reason-1', {
+    rotate:{x:80, y:0, z:0},
+    duration: 1000,
+})
+sr.reveal('.reason-2', {
+    rotate:{x:80, y:0, z:0},
+    duration: 2000,
+})
+sr.reveal('.reason-3', {
+    rotate:{x:80, y:0, z:0},
+    duration: 3000,
+})
+
+sr.reveal('.card-services-1', {
+    rotate:{x:0, y:80, z:0},
+    duration: 1000,
+})
+sr.reveal('.card-services-2', {
+    rotate:{x:0, y:80, z:0},
+    duration: 1000,
+})
+sr.reveal('.card-services-3', {
+    rotate:{x:0, y:80, z:0},
+    duration: 1000,
+})
+sr.reveal('.video-1', {
+    rotate:{x:80, y:0, z:0},
+    duration: 1000,
+})
+sr.reveal('.video-2', {
+    rotate:{x:80, y:0, z:0},
+    duration: 1000,
+})
