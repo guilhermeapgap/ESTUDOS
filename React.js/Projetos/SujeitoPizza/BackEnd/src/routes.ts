b68800e663d8b1ef6ebc19e91affe90b1e3bbb9a
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-02-22 12:23:22 -0300

    backend sujeito pizza

--- React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
+++ React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response} from 'express'
 
 import { CreateUserController } from './controllers/user/createUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router()
 
 
 router.post('/users', new CreateUserController().handle)
 
+router.post('/session', new AuthUserController().handle)
 
-export {router}
+export { router }
