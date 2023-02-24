a0df29f3987eee2cff5f10ea04bf4117e13c914d
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-02-24 20:14:27 -0300

    back end sujeito pizza

--- React.js/Projetos/SujeitoPizza/BackEnd/src/controllers/user/DetaillUserController.ts
+++ React.js/Projetos/SujeitoPizza/BackEnd/src/controllers/user/DetaillUserController.ts
@@ -3,8 +3,12 @@ import { DetaillUserService } from "../../services/user/DetaillUserService";
 
 class DetaillUserController{
     async handle(req:Request, res: Response){
+
+        const user_id = req.user_id
+
         const detaillUserService = new DetaillUserService();
-        const user = await detaillUserService.execute()
+
+        const user = await detaillUserService.execute(user_id)
 
         return res.json(user)
     }
