89a2f4a08f601259b4b45929e9723df297c06477
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-03-01 20:24:54 -0300

    back end sujeito pizza

--- React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
+++ React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response} from 'express'
+import multer from 'multer'
 
 import { CreateUserController } from './controllers/user/createUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
@@ -8,10 +9,18 @@ import { CreateCategoryController } from './controllers/Category/CreateCategoryC
 import { ListCategoryController } from './controllers/Category/ListCategoryController'
 
 import { CreateProductController } from './controllers/products/CreateProductController'
+import { ListByCategoryController } from './controllers/products/ListByCategoryController'
+
+import { CreateOrderController } from './controllers/order/CreateOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
+import uploadConfig from './config/multer'
+
 const router = Router()
+
+const upload = multer(uploadConfig.upload("./tmp"))
+
 
 // Rotas user
 router.post('/users', new CreateUserController().handle)
@@ -26,5 +35,13 @@ router.post('/category' , isAuthenticated, new CreateCategoryController().handle
 router.get('/category' , isAuthenticated, new ListCategoryController().handle)
 
 // Rotas product
-router.post('/product', isAuthenticated, new CreateProductController().handle)
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+
+// Rotas order
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
+
 export { router }
