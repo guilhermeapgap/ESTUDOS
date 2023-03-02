22cad1286511cfeea556a228a2287a0531a06cbc
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-03-02 20:08:43 -0300

    back end

--- React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
+++ React.js/Projetos/SujeitoPizza/BackEnd/src/routes.ts
@@ -12,6 +12,9 @@ import { CreateProductController } from './controllers/products/CreateProductCon
 import { ListByCategoryController } from './controllers/products/ListByCategoryController'
 
 import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RevomeItemController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -43,5 +46,10 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // Rotas order
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 export { router }
