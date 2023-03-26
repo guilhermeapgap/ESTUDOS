12f12f64ea1b17a73e4ee08527a5567ef0adc349
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-03-26 18:54:47 -0300

    front

--- React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/home.module.scss'
 import logoImg from '../../public/logo.svg'
 
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/Button"
 
 export default function Home() {
   return (
@@ -28,7 +29,12 @@ export default function Home() {
             type="password"
           />
 
-          
+          <Button
+            type="submit"
+            loading={false} 
+          >
+            Cadastrar
+          </Button>
         </form>
       </div>
     </div>
