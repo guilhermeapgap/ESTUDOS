528bb40c525eb72266855f5e3cd0b7a92dad8bf8
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-03-31 21:37:28 -0300

    asllan

--- Projetos/asllan/src/components/footer/index.js
+++ Projetos/asllan/src/components/footer/index.js
@@ -5,7 +5,38 @@ import styles from './footer.module.scss';
 function Footer() {
   return (
     <div className={styles.container}>
-        <h1>footer</h1>
+        <div className={styles.box01}>
+          <div>
+            <h1>Asllan</h1>
+            <a>Sobre</a>
+            <a>Nossos Principios</a>
+          </div>
+          <div>
+            <h1>Atendimento</h1>
+            <a>Dúvidas Frequentes</a>
+            <a>(61)9999-9999</a>
+            <a>email@mail.com</a>
+          </div>
+          <div>
+            <h1>Horário de atendimento</h1>
+            <p>Sábado - 08:00 às 19:00</p>
+            <p>Domingo - Fechado</p>
+            <p>Segunda-feira - 08:00 às 19:00</p>
+            <p>Terça-feira - 08:00 às 19:00</p>
+            <p>Quarta-feira - 08:00 às 19:00</p>
+            <p>Quinta-feira - 08:00 às 19:00</p>
+            <p>Sexta-feita - 08:00 às 19:00</p>
+          </div>
+        </div>
+        <div className={styles.box_media}>
+            <div>Redes</div>
+            <ul>
+              <li>ins</li>
+              <li>face</li>
+              <li>whats</li>
+              <li>tel</li>
+            </ul>
+        </div>
     </div>
   );
 }
---
+++ Projetos/asllan/src/components/navbar/index.js
@@ -0,0 +1,13 @@
+
+import styles from './navbar.module.scss';
+
+
+function Navbar() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Navbar;
