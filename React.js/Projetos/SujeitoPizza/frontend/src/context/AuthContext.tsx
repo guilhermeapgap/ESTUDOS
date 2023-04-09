ca00176782aa0b57e5905ecdae14394c75e7be01
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-04-08 21:38:37 -0300

    dashboard

--- React.js/Projetos/SujeitoPizza/frontend/src/context/AuthContext.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/context/AuthContext.tsx
@@ -1,10 +1,16 @@
 import { createContext, ReactNode, use, useState } from 'react'
 
+import { api } from '@/services/apiClient';
+
+
+import { destroyCookie ,setCookie, parseCookies} from 'nookies';
+import Router from 'next/router';
 
 type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -24,18 +30,57 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut(){
+    try{
+        destroyCookie(undefined,'cc')
+        Router.push('/')
+    }catch{
+        console.log('erro ao deslogar')
+    }
+}
+
+
 export function AuthProvider({ children }: AuthProviderProps){
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
 
 
     async function signIn({email,password}:SignInProps){
-        console.log('dados para logar', email)
-        console.log('Senha ', password)
+        try{
+            const respose = await api.post('/session',{
+                email,
+                password
+            })
+
+            //console.log(respose.data)
+            const {id,name,token} = respose.data
+            
+            setCookie(undefined, '@nextauth.token' , token, {
+                maxAge: 60 * 60 * 24 * 30,
+                path:"/"// quais caminhos terao acesso ao cookie
+            })
+
+            setUser({
+                id,
+                name,
+                email,
+            })
+
+            //passar para proximos requisições o nosso token 
+            api.defaults.headers['Authorization']= `Bearer ${token}`
+
+
+            //redirecionar o user para a /dashboard
+            Router.push('/dashboard')
+
+            
+        }catch(err){
+            console.log('erro ao acessar ', err)
+        }
     }
 
     return(
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
           {children}
         </AuthContext.Provider>
       )
