61e932c167d463ac27e0be5ff8629d3d47ebfdf1
Guilherme <Guilhermeapgap@hotmail.com>
2023-04-12 22:02:58 -0300

    commit

--- React.js/Projetos/SujeitoPizza/frontend/src/pages/dashboard/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/dashboard/index.tsx
@@ -2,6 +2,8 @@ export default function Dashboard(){
     return(
         <div>
             <h1>Bem vindo ao painel</h1>
+            <h2>aqui e meu h2 </h2>
+            <p>aqui o P</p>
         </div>
     )
 }
--- React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
 
   const { signIn } = useContext(AuthContext)
 
+  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
