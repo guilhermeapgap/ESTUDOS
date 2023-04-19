cc5d70c6c7f1aab2487a4ee1d3e16f0e7f987752
Guilherme <Guilhermeapgap@hotmail.com>
2023-04-18 21:56:45 -0300

    commit

--- React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
   const { signIn } = useContext(AuthContext)
 
   
+  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -54,7 +55,7 @@ export default function Home() {
             value={email}
             onChange= { (e)=> setEmail(e.target.value)}
           />
-
+          <h1>oi</h1>
           
           <Input 
             placeholder="Digite sua senha"
