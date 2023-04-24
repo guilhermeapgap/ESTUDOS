b959e443f5d12f5e3cb35a46b4e723626280768f
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-04-23 21:11:26 -0300

    finalizando parte de cardapio

--- React.js/Projetos/SujeitoPizza/frontend/src/pages/product/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/product/index.tsx
@@ -1,4 +1,4 @@
-import {useState, ChangeEvent} from 'react'
+import {useState, ChangeEvent, FormEvent} from 'react'
 import Head from "next/head";
 import { Header } from "@/components/Header";
 import styles from './styles.module.scss'
@@ -7,6 +7,7 @@ import { canSSRAuth } from "@/utils/canSSRAuth";
 import { FiUpload } from "react-icons/fi";
 
 import { setupAPIClient } from '@/services/api';
+import { toast } from 'react-toastify';
 
 type ItemProps = {
     id: string;
@@ -19,6 +20,9 @@ interface CategoryProps{
 
 
 export default function Product({categoryList}: CategoryProps){
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [description, setDescripton] = useState('')
 
     const [avatarUrl, setAvatarUrl] = useState('')
     const [imageAvatar, setImageAvatar] = useState(null)
@@ -46,9 +50,44 @@ export default function Product({categoryList}: CategoryProps){
     function handleChangeCategory(event){
 
       setCategorySelected(event.target.value)  
-      
     }
 
+
+
+    async function handleRegister(event: FormEvent){
+        event.preventDefault()
+      
+        try{  
+            const data = new FormData()
+
+            if(name === '' || price === '' || description === '' || imageAvatar === null){
+                toast.error('Preencha todos os campos!')
+                return
+            }
+
+            data.append('name', name)
+            data.append('price', price)
+            data.append('description', description)
+            data.append('category_id', categories[categorySelected].id)
+            data.append('file', imageAvatar)
+
+            const apiClient = setupAPIClient()
+
+            await apiClient.post('/product', data)
+
+            toast.success('Cadastrado com sucesso!')
+
+        }catch(err){
+            console.log(err)
+            toast.error('Ops erro ao cadastrar!')
+        }
+
+        setName('')
+        setPrice('')
+        setDescripton('')
+        setImageAvatar(null)
+        setAvatarUrl('')
+    }
     return(
         <>
         <Head>
@@ -59,7 +98,7 @@ export default function Product({categoryList}: CategoryProps){
             <main className={styles.container}>
                 <h1>Novo produto</h1>
 
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleRegister}>
                     
                     <label className={styles.labelAvatar}>
                         <span>
@@ -93,17 +132,23 @@ export default function Product({categoryList}: CategoryProps){
                     type="text"
                     placeholder="Digite o nome do produto"
                     className={styles.input}
+                    value={name}
+                    onChange={(e)=> setName(e.target.value)}
                     />
 
                     <input 
                     type="text"
                     placeholder="Preço do produto"
                     className={styles.input}
+                    value={price}
+                    onChange={(e)=> setPrice(e.target.value)}
                     />
 
                     <textarea 
                     placeholder="Descreva seu produto..."
                     className={styles.input}
+                    value={description}
+                    onChange={(e)=> setDescripton(e.target.value)}
                     />
 
                     <button className={styles.buttonAdd} type="submit">
