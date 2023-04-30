d0622192fddfa0d1b70bd930d06c83a6eb057e82
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-04-29 21:45:37 -0300

    modal

--- React.js/Projetos/SujeitoPizza/frontend/src/components/ModalOrder/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/components/ModalOrder/index.tsx
@@ -1,3 +1,15 @@
+import Modal from 'react-modal'
+import styles from './style.module.scss'
+
+import { FiX } from 'react-icons/fi'
+
+import { OrderItemProps } from '@/pages/dashboard'
+interface ModalOrderProps{
+    isOpen: boolean
+    onRequestClose: () => void
+    order: OrderItemProps[]
+}
+
 export function ModalOrder(){
     return(
         <div>
