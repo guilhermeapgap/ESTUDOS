9474f3f1eb7df3b99cacab0e9edb8d62efdfc8a7
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-04-30 21:07:40 -0300

    modal

--- React.js/Projetos/SujeitoPizza/frontend/src/pages/dashboard/index.tsx
+++ React.js/Projetos/SujeitoPizza/frontend/src/pages/dashboard/index.tsx
@@ -103,7 +103,11 @@ export default function Dashboard({orders}: HomeProps){
             </main>
 
            {modalVisible && (
-            <ModalOrder/>
+            <ModalOrder
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            order={modalItem}
+            />
            )}             
 
         </div>
