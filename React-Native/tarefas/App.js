7dfbceb54f0824b93075a01b97d010abde6234d6
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-05-05 20:21:50 -0300

    finalizando aplicativo de tarefas

--- React-Native/tarefas/App.js
+++ React-Native/tarefas/App.js
@@ -5,11 +5,41 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  FlatList,
  } from "react-native";
 
 import {FontAwesome} from '@expo/vector-icons'
 
+import Tarefas from "./src/Tarefa";
+
 export default function App(){
+  const [tarefa, setTarefa] = useState('')
+
+  const [list, setList] = useState([])
+  function handleAdd(){
+      
+    if(tarefa === ''){
+        return
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa
+    }
+
+    setList(oldArray => [dados, ...oldArray])
+
+    setTarefa ('')
+  }
+
+  function handleDelete(item){
+    let filtroItem = list.filter((tarefa) =>{
+      return (tarefa.item !== item)
+    })
+
+    setList(filtroItem)
+    
+  }
   return(
     <View style={styles.container}>
       <Text style={styles.title}>Tarefas</Text>
@@ -18,13 +48,21 @@ export default function App(){
         <TextInput
         placeholder="Digite sua tarfa..."
         style={styles.input}
+        value={tarefa}
+        onChangeText={ (text) => setTarefa(text)}
         />
 
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
           <FontAwesome name="plus" size={20} color="#fff"/>
         </TouchableOpacity>
       </View>
 
+      <FlatList
+        data={list}
+        keyExtractor={ (item) => item.key}
+        renderItem={ ({item}) => <Tarefas data={item} deleteItem={ () => handleDelete(item.item)}/>}
+        style={styles.list}
+      />
 
     </View>
   )
@@ -36,7 +74,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#22272e',
     paddingTop: 28,
   },
-
   title:{
     fontWeight:'bold',
     fontSize: 24,
@@ -47,5 +84,32 @@ const styles = StyleSheet.create({
   },
   containerInput:{
     flexDirection: 'row',
+    width:'100%',
+    height:44,
+    alignItems:'center',
+    justifyContent: 'center',
+    marginBottom: 22,
   },
+  input:{
+    width:'75%',
+    backgroundColor:'#fbfbfb',
+    height: 44,
+    borderRadius:4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd:{
+    width:'15%',
+    height: 44,
+    backgroundColor:'#73f7ff',
+    marginLeft:8,
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius: 4,
+  },
+  list:{
+    flex:1,
+    backgroundColor:'#fff',
+    paddingStart:'4%',
+    paddingEnd:'4%',
+  }
 })
