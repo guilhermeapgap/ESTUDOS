085ea2d5cb984a2d8e8a1ef0055d4072727792fc
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-05-04 21:42:33 -0300

    aplicativo tarefas

--- React-Native/tarefas/App.js
+++ React-Native/tarefas/App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { 
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+ } from "react-native";
 
-export default function App() {
-  return (
+import {FontAwesome} from '@expo/vector-icons'
+
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tarefas</Text>
+
+      <View style={styles.containerInput}>
+        <TextInput
+        placeholder="Digite sua tarfa..."
+        style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.buttonAdd}>
+          <FontAwesome name="plus" size={20} color="#fff"/>
+        </TouchableOpacity>
+      </View>
+
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    backgroundColor: '#22272e',
+    paddingTop: 28,
   },
-});
+
+  title:{
+    fontWeight:'bold',
+    fontSize: 24,
+    color:'#fff',
+    marginTop:'5%',
+    paddingStart:'5%',
+    marginBottom: 12,
+  },
+  containerInput:{
+    flexDirection: 'row',
+  },
+})
