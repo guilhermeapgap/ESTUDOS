9a262e0d1882f18971c585d468334a1a5fb01d5f
Guilherme <Guilhermeapgap@hotmail.com>
2023-05-10 17:28:42 -0300

    tela de login

--- React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
+++ React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function SignIn(){
   return(
     <View>
-      <Text>Tela de login!!</Text>
+      <Text>Tela de login!!!!</Text>
     </View>
   )
 }
