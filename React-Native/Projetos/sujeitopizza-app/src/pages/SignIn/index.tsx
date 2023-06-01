cf270184c73bd1c301c7e4067e233fc29ff639fd
Guilherme <Guilhermeapgap@hotmail.com>
2023-05-31 22:52:51 -0300

    'cmt'

--- React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
+++ React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
@@ -32,7 +32,7 @@ export default function SignIn(){
 
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Digite seu emaill:"   
+          placeholder="Digite seu email:"   
           style={styles.input}     
           placeholderTextColor="#F0F0F0"
           value={email}
@@ -41,7 +41,7 @@ export default function SignIn(){
 
 
         <TextInput
-          placeholder="Sua senha:"      
+          placeholder="Sua senhaq:"      
           style={styles.input}   
           placeholderTextColor="#F0F0F0"
           secureTextEntry={true}
