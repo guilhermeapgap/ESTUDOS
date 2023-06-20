21c40a089817865fb81063a1dc94a00fbe119b37
Guilherme <Guilhermeapgap@hotmail.com>
2023-06-20 20:21:06 -0300

    adicionando context e modificando a pagina de dashboard

--- React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
+++ React-Native/Projetos/sujeitopizza-app/src/pages/SignIn/index.tsx
@@ -6,13 +6,14 @@ import {
   StyleSheet,
   Image,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native'
 
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function SignIn(){
-  const {signIn} = useContext(AuthContext)
+  const {signIn, loadingAuth} = useContext(AuthContext)
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -52,7 +53,12 @@ export default function SignIn(){
         />       
 
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Acessssar</Text>
+          {loadingAuth ? (
+            <ActivityIndicator size={25} color={"#fff"}/>
+          ): (
+            <Text style={styles.buttonText}>Acessssar</Text>
+          )}
+          
         </TouchableOpacity>    
       </View>
 
