f37a7d7d273bcab4df85d7626fdd4589fbc07b63
Guilherme <Guilhermeapgap@hotmail.com>
2023-06-21 19:46:42 -0300

    pagina de order

--- React-Native/Projetos/sujeitopizza-app/src/pages/Dashboard/index.tsx
+++ React-Native/Projetos/sujeitopizza-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+
+import {useNavigation} from '@react-navigation/native'
+
+import { StackPromsList } from '../../routes/app.routes';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
 
 
 export default function Dashboard(){
+  const navigation = useNavigation<NativeStackNavigationProp<StackPromsList>>()
 
+  const [number, setNumber] = useState('')
+
+  async function openOrder(){
+    if(number === ''){
+      return
+    }
+    //precisa fazer a requisição e abrir a mesa e navegar para proxima tela.
+    navigation.navigate('Order',{number:number, order_id:'86a5a9ba-5f0f-4f46-a9e7-5d80448e9ef7' })
+  }
   return(
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Novo pedido</Text>
@@ -13,9 +29,11 @@ export default function Dashboard(){
         placeholderTextColor={'#f0f0f0'}
         style={styles.input}
         keyboardType='numeric'
+        value={number}
+        onChangeText={setNumber}
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={openOrder}>
         <Text style={styles.buttonText}> Abrir mesa </Text>
       </TouchableOpacity>
     </SafeAreaView>
@@ -42,7 +60,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#101026',
     borderRadius: 4,
     paddingHorizontal: 8,
-    textAlign:'center',
+    //textAlign:'center',
     fontSize: 22,
     color: '#fff'
   },
