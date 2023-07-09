58ab9912c7d8b62eacfd72d15ed4b33dae1064be
Guilherme <Guilhermeapgap@hotmail.com>
2023-07-08 22:51:05 -0300

    .

--- React-Native/Projetos/sujeitopizza-app/src/services/api.ts
+++ React-Native/Projetos/sujeitopizza-app/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({   
     //baseURL:'http:://localhost:3333'
 
-    //notebook//
+    //notebook
     baseURL:'http://192.168.100.97:3333'
 
     //desktop
